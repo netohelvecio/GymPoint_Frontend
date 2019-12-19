778c0d784e733b09eaff56e6d97ae5da1b6f8f59
Helvécio Neto <helvecioneto77@gmail.com>
2019-12-19 14:00:20 -0300

    config redux

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import createSagaMiddleware from 'redux-saga';
+// import { persistStore } from 'redux-persist';
+
+import createStore from './createStore';
+import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+// import persistReducers from './persistReducers';
+
+const middlewares = [];
+
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+middlewares.push(sagaMiddleware);
+
+const store = createStore(rootReducer, middlewares);
+// const persistor = persistStore(store);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
