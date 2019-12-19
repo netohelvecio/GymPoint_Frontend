0a639fb0dcdadbe2e60c4714aa48648fc36943fa
Helvécio Neto <helvecioneto77@gmail.com>
2019-12-19 11:21:32 -0300

    rotas finalizado

---
+++ src/pages/Plans/PlansRegister/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function PlansRegister() {
+  return <h1>register plan</h1>;
+}
---
+++ src/pages/Students/StudentsEdit/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function StudentsEdit() {
+  return <h1>student edit</h1>;
+}
